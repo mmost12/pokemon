f10f53d812b0dd6f6dea438cd732bffe78988357
Micah Most <Micah Most>
2016-10-08 14:55:58 -0500

    Continued EA work.

--- gap.py
+++ gap.py
@@ -1,4 +1,4 @@
-from random import randint
+from random
 
 # Sample Database row
 # Name	Type 1	Type 2	Total	HP	Attack	Defense	Sp. Attack	Sp. Def	Speed	Generation	Legendary
@@ -6,6 +6,7 @@ from random import randint
 # Global variables
 pop_size       = 100
 stop_evolution = 1000
+k_tournament   = 10
 population     = []
 
 def recombination(chrom1, chrom2):
@@ -18,10 +19,11 @@ def recombination(chrom1, chrom2):
 
 def mutation(chrom):
 	for individual in chrom:
-		rand = randint(1,100)
-		if(randint < 5):
-			rand_pokemon = randint(0,800)
+		rand = random.randint(1,100)
+		if(rand <= 5):
+			rand_pokemon = random.randint(0,800)
 			chrom[individual] = Pokemon.objects.all().filter(num=rand_pokemon)
+	return chrom
 
 def evaluate(chrom):
 	for individual in chrom:
@@ -29,12 +31,14 @@ def evaluate(chrom):
    return fitness
 
 # Main 
+random.seed()
+
 # Initilize population
 for x in range(0,pop_size):
 	chrom = []
 
 	for team_select in range(0,5):
-		rand = randint(0,800)
+		rand = random.randint(0,800)
 		chrom.append(Pokemon.objects.all().filter(num=rand))
 
 	population.append(chrom)
@@ -42,16 +46,23 @@ for x in range(0,pop_size):
 # Initial evaluation
 for generation in range(0,stop_evolution):
 	new_population = []
-	for chrom in range(0,pop_size):
+	for chrom in population:
 		# Select parents
-		# Recombination
-		child = recombination(chrom1, chrom2)
-		
-		# Mutation
-		mutation(child)
+		chrom1 = population[]
+		chrom2 = population[]
 
-		# Evaluation
-		new_population.append(chrom1)
+		# Recombination & Mutation
+		child   = mutation(recombination(chrom1,chrom2))
+		fitness = evaluate(child)
 
-	for chrom in range
-		# Select new population
+		fitness_pair = (fitness, child)
+		new_population.append(fitness_pair)
+
+	for chrom in new_population:
+		heap = []
+		for add in range(1,k_tournament):
+			rand_pokemon = random.randint(0,pop_size)
+			heap.append(new_population[rand_pokemon])
+		heap.sort(key=lambda tup: tup[0])
+
+		population[chrom] = heap[-1]
