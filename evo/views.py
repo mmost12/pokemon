a558f087ffd87f2af38a5833bf11256ab329b08c
wes <carlsonwes@gmail.com>
2016-10-09 08:43:08 -0500

    post redirect working

--- evo/views.py
+++ evo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
+app_name = 'evo'
 # Create your views here.
 def index(request):
-    return render(request, 'evo/index.html', {})
+    if request.method == 'POST':
+        return HttpResponseRedirect('evo/')
+    else:
+        return render(request, 'evo/index.html', {})
+
+def evo(request):
+    return render(request, 'evo/evo.html', {})
