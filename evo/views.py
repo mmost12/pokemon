f4172147df2f2154b7e5e36cd49146b0145ece79
wes <carlsonwes@gmail.com>
2016-10-09 13:28:37 -0500

    separated fix into its own function

--- evo/views.py
+++ evo/views.py
@@ -17,12 +17,18 @@ def fight(request):
     report = test.show_fight()
     teams  = report[:2]
     report = report[2:]
+    report = fix(report)
     return render(request, 'evo/fight.html', {'report': report, 'teams': teams})
 
 def evo(request):
     report = [x.split() for x in test.small()]
-    for i in range(len(report)):
-        report[i] = [x for x in report[i] if not x[len(x)-4:]=='Mega'
+    report = fix(report)
+
+    return render(request, 'evo/evo.html', {'report': report})
+
+def fix(l):
+    for i in range(len(l)):
+        l[i] = [x for x in l[i] if not x[len(x)-4:]=='Mega'
                                           and not x[len(x)-6:]=='Primal'
                                           and not x == 'Forme'
                                           and not x == 'Mode'
@@ -31,66 +37,67 @@ def evo(request):
                                           and not x == 'Mime'
                                           and not x == 'X'
                                           and not x == 'Y']
-        for j in range(len(report[i])):
-            if report[i][j][len(report[i][j])-3:] == 'Zen':
-                report[i][j] = report[i][j][:len(report[i][j])-3] + '-Zen'
-            elif report[i][j][len(report[i][j])-3:] == 'Fan':
-                report[i][j] = report[i][j][:len(report[i][j])-3] + '-Fan'
-            elif report[i][j][len(report[i][j])-3:] == 'Sky':
-                report[i][j] = report[i][j][:len(report[i][j])-3] + '-Sky'
-            elif report[i][j][len(report[i][j])-4:] == 'Aria':
-                report[i][j] = report[i][j][:len(report[i][j])-4] + '-Aria'
-            elif report[i][j][len(report[i][j])-5:] == 'Frost':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Frost'
-            elif report[i][j][len(report[i][j])-7:] == 'Altered':
-                report[i][j] = report[i][j][:len(report[i][j])-7] + '-Altered'
-            elif report[i][j][len(report[i][j])-7:] == 'Defense':
-                report[i][j] = report[i][j][:len(report[i][j])-7] + '-Defense'
-            elif report[i][j][len(report[i][j])-5:] == 'Large':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Large'
-            elif report[i][j][len(report[i][j])-4:] == 'Heat':
-                report[i][j] = report[i][j][:len(report[i][j])-4] + '-Heat'
-            elif report[i][j][len(report[i][j])-4:] == 'Wash':
-                report[i][j] = report[i][j][:len(report[i][j])-4] + '-Wash'
-            elif report[i][j][len(report[i][j])-5:] == 'Small':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Small'
-            elif report[i][j][len(report[i][j])-5:] == 'Trash':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Trash'
-            elif report[i][j][len(report[i][j])-5:] == 'White':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-White'
-            elif report[i][j][len(report[i][j])-5:] == 'Plant':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Plant'
-            elif report[i][j][len(report[i][j])-5:] == 'Sandy':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Sandy'
-            elif report[i][j][len(report[i][j])-5:] == 'Super':
-                report[i][j] = report[i][j][:len(report[i][j])-5] + '-Super'
-            elif report[i][j][len(report[i][j])-6:] == 'Female':
-                report[i][j] = report[i][j][:len(report[i][j])-6] + '-Female'
-            elif report[i][j][len(report[i][j])-6:] == 'Normal':
-                report[i][j] = report[i][j][:len(report[i][j])-6] + '-Normal'
-            elif report[i][j][len(report[i][j])-7:] == 'Average':
-                report[i][j] = report[i][j][:len(report[i][j])-7] + '-Average'
-            elif report[i][j][len(report[i][j])-6:] == 'Shield':
-                report[i][j] = report[i][j][:len(report[i][j])-6] + '-Shield'
-            elif report[i][j][len(report[i][j])-7:] == 'Therian':
-                report[i][j] = report[i][j][:len(report[i][j])-7] + '-Therian'
-            elif report[i][j][len(report[i][j])-8:] == 'Resolute':
-                report[i][j] = report[i][j][:len(report[i][j])-8] + '-Resolute'
-            elif report[i][j][len(report[i][j])-8:] == 'Standard':
-                report[i][j] = report[i][j][:len(report[i][j])-8] + '-Standard'
-            elif report[i][j][len(report[i][j])-8:] == 'Ordinary':
-                report[i][j] = report[i][j][:len(report[i][j])-8] + '-Ordinary'
-            elif report[i][j][len(report[i][j])-4:] == 'Land':
-                report[i][j] = report[i][j][:len(report[i][j])-4] + '-Land'
-            elif report[i][j][len(report[i][j])-9:] == 'Incarnate':
-                report[i][j] = report[i][j][:len(report[i][j])-9] + '-Incarnate'
-            elif report[i][j][len(report[i][j])-9:] == 'Pirouette':
-                report[i][j] = report[i][j][:len(report[i][j])-9] + '-Pirouette'
-            elif report[i][j] == 'Farfetch\'d':
-                report[i][j] = 'Farfetchd'
-            elif report[i][j] == 'Mr.':
-                report[i][j] = 'Mr-Mime'
-            elif report[i][j] == 'Jr.':
-                report[i][j] = 'Mime-Jr'
+        for j in range(len(l[i])):
+            l[i][j] = l[i][j].split()[0]
+            if l[i][j][len(l[i][j])-3:] == 'Zen':
+                l[i][j] = l[i][j][:len(l[i][j])-3] + '-Zen'
+            elif l[i][j][len(l[i][j])-3:] == 'Fan':
+                l[i][j] = l[i][j][:len(l[i][j])-3] + '-Fan'
+            elif l[i][j][len(l[i][j])-3:] == 'Sky':
+                l[i][j] = l[i][j][:len(l[i][j])-3] + '-Sky'
+            elif l[i][j][len(l[i][j])-4:] == 'Aria':
+                l[i][j] = l[i][j][:len(l[i][j])-4] + '-Aria'
+            elif l[i][j][len(l[i][j])-5:] == 'Frost':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Frost'
+            elif l[i][j][len(l[i][j])-7:] == 'Altered':
+                l[i][j] = l[i][j][:len(l[i][j])-7] + '-Altered'
+            elif l[i][j][len(l[i][j])-7:] == 'Defense':
+                l[i][j] = l[i][j][:len(l[i][j])-7] + '-Defense'
+            elif l[i][j][len(l[i][j])-5:] == 'Large':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Large'
+            elif l[i][j][len(l[i][j])-4:] == 'Heat':
+                l[i][j] = l[i][j][:len(l[i][j])-4] + '-Heat'
+            elif l[i][j][len(l[i][j])-4:] == 'Wash':
+                l[i][j] = l[i][j][:len(l[i][j])-4] + '-Wash'
+            elif l[i][j][len(l[i][j])-5:] == 'Small':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Small'
+            elif l[i][j][len(l[i][j])-5:] == 'Trash':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Trash'
+            elif l[i][j][len(l[i][j])-5:] == 'White':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-White'
+            elif l[i][j][len(l[i][j])-5:] == 'Plant':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Plant'
+            elif l[i][j][len(l[i][j])-5:] == 'Sandy':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Sandy'
+            elif l[i][j][len(l[i][j])-5:] == 'Super':
+                l[i][j] = l[i][j][:len(l[i][j])-5] + '-Super'
+            elif l[i][j][len(l[i][j])-6:] == 'Female':
+                l[i][j] = l[i][j][:len(l[i][j])-6] + '-Female'
+            elif l[i][j][len(l[i][j])-6:] == 'Normal':
+                l[i][j] = l[i][j][:len(l[i][j])-6] + '-Normal'
+            elif l[i][j][len(l[i][j])-7:] == 'Average':
+                l[i][j] = l[i][j][:len(l[i][j])-7] + '-Average'
+            elif l[i][j][len(l[i][j])-6:] == 'Shield':
+                l[i][j] = l[i][j][:len(l[i][j])-6] + '-Shield'
+            elif l[i][j][len(l[i][j])-7:] == 'Therian':
+                l[i][j] = l[i][j][:len(l[i][j])-7] + '-Therian'
+            elif l[i][j][len(l[i][j])-8:] == 'Resolute':
+                l[i][j] = l[i][j][:len(l[i][j])-8] + '-Resolute'
+            elif l[i][j][len(l[i][j])-8:] == 'Standard':
+                l[i][j] = l[i][j][:len(l[i][j])-8] + '-Standard'
+            elif l[i][j][len(l[i][j])-8:] == 'Ordinary':
+                l[i][j] = l[i][j][:len(l[i][j])-8] + '-Ordinary'
+            elif l[i][j][len(l[i][j])-4:] == 'Land':
+                l[i][j] = l[i][j][:len(l[i][j])-4] + '-Land'
+            elif l[i][j][len(l[i][j])-9:] == 'Incarnate':
+                l[i][j] = l[i][j][:len(l[i][j])-9] + '-Incarnate'
+            elif l[i][j][len(l[i][j])-9:] == 'Pirouette':
+                l[i][j] = l[i][j][:len(l[i][j])-9] + '-Pirouette'
+            elif l[i][j] == 'Farfetch\'d':
+                l[i][j] = 'Farfetchd'
+            elif l[i][j] == 'Mr.':
+                l[i][j] = 'Mr-Mime'
+            elif l[i][j] == 'Jr.':
+                l[i][j] = 'Mime-Jr'
 
-    return render(request, 'evo/evo.html', {'report': report})
+    return l
